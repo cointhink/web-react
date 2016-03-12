39bbd5abf70292cca3284070fabeb567d31b22fc
Don Park <donp@donp.org>
2016-03-12 09:12:34 -0800

    jade

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,29 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jade: {
+      compile: {
+        options: {
+          pretty: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'jade',
+          src: ['*.jade'],
+          dest: 'build',
+          ext: '.html'
+        }]
+      }
+    },
     browserify: {
-         dist: {
-            options: {
-               transform: [
-                  ["babelify", {}]
-               ]
-            },
-            files: { "./build/bundle.js": "./js/*.js"}
-         }
+      dist: {
+        options: {
+           transform: [
+              ["babelify", {}]
+           ]
+        },
+        files: { "./build/bundle.js": "./js/*.js"}
+      }
     },
     copy: {
       main: {
@@ -25,8 +39,9 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-browserify')
   grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.loadNpmTasks('grunt-contrib-jade')
 
   // Default task(s).
-  grunt.registerTask('default', ['browserify', 'copy']);
+  grunt.registerTask('default', ['browserify', 'jade', 'copy']);
 
 }
